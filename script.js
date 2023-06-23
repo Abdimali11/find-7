96a028a2aef91343bfe7aec21783b8984f16b131
Abdimalik11 <ilkacaze5522@gmail.com>
2023-06-23 10:13:52 -0700

    this is the challenge of FIND-7

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 // Good Luck 💪🏾
+//findSeven([1, 2, 3, 4, 5, 6, 7])
+function findSeven(numbers) {
+    for (let i = 0; i < numbers.length; i++) {
+      if (numbers[i] === 7) {
+        return "num 7 is Founded!";
+      }
+    }
+    return "There is no 7 in the array.";
+  }
+  const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+console.log(findSeven(numbers)); // Output: Found!
+
+const otherNumbers = [1, 2, 3, 4, 5, 6, 8, 9];
+console.log(findSeven(otherNumbers)); // Output: There is no 7 in the array.
